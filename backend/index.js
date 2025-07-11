1ff1b7cb9f74efc8162788931e3d1aee1e235375
Nakul Gadhewal <164036255+nakulgadhewal050@users.noreply.github.com>
2025-07-11 23:54:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ import orderRoute from './routes/orderRoute.js';
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:['https://e-commerce-frontend-sypc.onrender.com','http://localhost:5174'],
+    origin:['https://e-commerce-frontend-sypc.onrender.com','https://e-commerce-admin-z4kz.onrender.com'],
     credentials: true,
 
 }))
