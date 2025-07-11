32f2b7da223b249a7f3b3e65816b01967f59e97f
nakulgadhewal <nakulgadhewal050@gmail.com>
2025-07-12 00:06:59 +0530

    token change

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const isAuth = (req, res, next) => {
     try {
-        let {token} = req.cookies;
+        let token = req.cookies.token
 
         if(!token){
             return res.status(401).json({message: "Unauthorized access"});
